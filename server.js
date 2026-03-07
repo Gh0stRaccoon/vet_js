da9af736777dbbf4f008226fe811a069648eeddc
fsjs-vet <fullstackjavasciptveterinariob@gmail.com>
2026-03-06 22:32:08 -0300

    vercl configs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { ownersRoutes } = require("./src/routes/ownersRoutes");
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-server.use("/api/pets", petsRoutes);
+// server.use("/api/pets", petsRoutes);
 server.use("/api/owners", ownersRoutes);
 
 module.exports = { server };
