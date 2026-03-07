b4677dd984d93e86fcf3c9939b13a81a7829adfa
fsjs-vet <fullstackjavasciptveterinariob@gmail.com>
2026-03-06 21:00:51 -0300

    configs

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,13 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URI, {
   // logging: false,
-  ssl: true,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = { sequelize };
